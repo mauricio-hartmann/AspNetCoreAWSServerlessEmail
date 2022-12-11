4dab70d6cdc15ca65852b3610d93fcba604080b7
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-12-11 10:34:42 -0300

    Envio de email com função lambda
    Ajuste na função lambda para enviar o email utilizando o SES.

--- src/lambdas/EmailFunction/Function.cs
+++ src/lambdas/EmailFunction/Function.cs
@@ -1,5 +1,9 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SNSEvents;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using ANCASE.Core.DTOs;
+using System.Text.Json;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -7,12 +11,29 @@ namespace EmailFunction;
 
 public class Function
 {
-    public void FunctionHandler(SNSEvent snsEvent)
+    public async Task FunctionHandlerAsync(SNSEvent snsEvent)
     {
         foreach (var record in snsEvent.Records)
         {
-            var snsRecord = record.Sns;
-            LambdaLogger.Log(snsRecord.Message);
+            try
+            {
+                var snsRecord = record.Sns;
+                EmailDTO email = JsonSerializer.Deserialize<EmailDTO>(snsRecord.Message);
+
+                var destination = new Destination(email.To.ToList());
+                var body = new Body(new Content(email.Body));
+                var emailMessage = new Message(new Content(email.Subject), body);
+                var emailRequest = new SendEmailRequest("mauricio.auditore@gmail.com", destination, emailMessage);
+                
+                var emailClient = new AmazonSimpleEmailServiceClient();
+                await emailClient.SendEmailAsync(emailRequest);
+
+                LambdaLogger.Log($"Email enviado: {snsRecord.Message}");
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log($"Erro ao enviar email: {e.Message}");
+            }
         }
     }
 }
